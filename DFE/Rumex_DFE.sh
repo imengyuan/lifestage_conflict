1549960cb76d716ebc48ee863d40fe4f2ef8362f
yuan <viciayuan@hotmail.com>
2023-10-10 23:57:47 -0400

    update expression scripts

--- DFE/Rumex_DFE.sh
+++ DFE/Rumex_DFE.sh
@@ -1,47 +1,39 @@
 # get input VCF.txt for generating SFS
 # /ohta2/meng.yuan/rumex/diversity2022/VCFs
-# l_specific_noTE_DE.bed
-# p_specific_noTE_DE.bed
-#for i in "p_specific_noTE_DE" "l_specific_noTE_DE"
-for i in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
-do
-bcftools view -R ${i}.bed eqtl20females.syn.filt0329.vcf.gz \
--O v -o ${i}.syn.filt0329.vcf --threads 30 
-bcftools view -R ${i}.bed eqtl20females.nonsyn.filt0329.vcf.gz \
--O v -o ${i}.nonsyn.filt0329.vcf --threads 30 
-done
 
-# "p_specific_noTE_DE" "l_specific_noTE_DE"
-for i in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+# for i in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+for i in "genesl" "genesp"
 do
+bcftools view -R ./bed/${i}.bed eqtl20females.syn.filt0329.vcf.gz \
+-O v -o ${i}.syn.filt0329.vcf --threads 30 
+bcftools view -R ./bed/${i}.bed eqtl20females.nonsyn.filt0329.vcf.gz \
+-O v -o ${i}.nonsyn.filt0329.vcf --threads 30 
+
 bcftools query -f '%CHROM\t%POS[\t%GT]\n' ${i}.syn.filt0329.vcf \
 |sed 's/\//\t/g' > ${i}.syn.filt0329.vcf.txt
 bcftools query -f '%CHROM\t%POS[\t%GT]\n' ${i}.nonsyn.filt0329.vcf \
 |sed 's/\//\t/g' > ${i}.nonsyn.filt0329.vcf.txt
 done
 
-# mv l_specific_noTE_DE*.vcf.txt ../bootstrap/
-# mv p_specific_noTE_DE*.vcf.txt ../bootstrap/
-for i in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+
+#for i in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+for i in "genesl" "genesp"
 do
 mv ${i}*.vcf.txt ../bootstrap/
 done
 
-# for i in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
-# do
-# wc -l ${i}*.vcf.txt 
-# done
-
 
 # get SFS
 cd /ohta2/meng.yuan/rumex/diversity2022/bootstrap
-for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+#for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+for sample in "genesl" "genesp" 
 do
 /usr/local/bin/Rscript --vanilla sfs.R ${sample}
 done
 
 
-for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+# for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+for sample in "genesl" "genesp" 
 do
 /usr/local/bin/Rscript --vanilla sfs_bootstrap.R ${sample}
 mv ${sample}*.sfs_*.txt bootstrap_specific/
@@ -50,7 +42,8 @@ done
 
 # run DFE
 # real data
-for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+# for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+for sample in "genesl" "genesp" 
 do
 cd /ohta2/meng.yuan/rumex/diversity2022/bootstrap
 cat head.txt ${sample}.nonsyn.sfs.txt ${sample}.syn.sfs.txt>../dfe_a/sfs_expressed_test.txt
@@ -63,7 +56,8 @@ done
 
 # rerun this too
 # bootstrap data
-for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+# for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4"
+for sample in "genesl" "genesp" 
 do
 for i in {1..200}; do
 # generate sfs.txt for input
