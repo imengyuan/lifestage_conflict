e7a46472140b2c18c19006488dbbe181aac3e58d
yuan <viciayuan@hotmail.com>
2024-04-11 13:38:31 -0400

    update DFE scripts

---
+++ DFE/run.dfe.sh
@@ -0,0 +1,48 @@
+# get input VCF.txt for generating SFS
+
+cd /ohta2/meng.yuan/rumex/diversity2022/VCFs
+for i in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4" "u_qt1" "u_qt2" "u_qt3" "u_qt4" "unbias" "genesl" "genesp"
+do
+bcftools view -R ./bed/${i}.bed eqtl20females.filt0329.syn.nomiss.vcf.gz \
+-O v -o ${i}.syn.filt0329.nomiss.vcf --threads 30 
+bcftools view -R ./bed/${i}.bed eqtl20females.filt0329.nonsyn.nomiss.vcf.gz \
+-O v -o ${i}.nonsyn.filt0329.nomiss.vcf --threads 30 
+
+bcftools query -f '%CHROM\t%POS[\t%GT]\n' ${i}.syn.filt0329.nomiss.vcf \
+|sed 's/\//\t/g' > ${i}.syn.filt0329.vcf.txt
+bcftools query -f '%CHROM\t%POS[\t%GT]\n' ${i}.nonsyn.filt0329.nomiss.vcf \
+|sed 's/\//\t/g' > ${i}.nonsyn.filt0329.vcf.txt
+
+mv ${i}*.vcf.txt ../bootstrap/
+done
+
+
+# get SFS
+cd /ohta2/meng.yuan/rumex/diversity2022/bootstrap
+for sample in "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4" "u_qt1" "u_qt2" "u_qt3" "u_qt4" "unbias" "genesl" "genesp"
+do
+/usr/local/bin/Rscript --vanilla sfs_bootstrap.R ${sample}
+mv ${sample}*.sfs_*.txt bootstrap_specific/
+done
+
+
+# run DFE
+# bootstrap data
+for sample in "unbias" "genesl" "genesp" "p_qt1" "p_qt2" "p_qt3" "p_qt4" "l_qt1" "l_qt2" "l_qt3" "l_qt4" "u_qt1" "u_qt2" "u_qt3" "u_qt4" 
+do
+for i in {1..200}; do
+# generate sfs.txt for input
+cd /ohta2/meng.yuan/rumex/diversity2022/bootstrap
+cat head.txt bootstrap_specific/${sample}.nonsyn.sfs_${i}.txt \
+bootstrap_specific/${sample}.syn.sfs_${i}.txt>../dfe_a/sfs_expressed_test.txt
+# run est_dfe
+cd /ohta2/meng.yuan/rumex/diversity2022/dfe_a/
+est_dfe -c est_dfe_configfile_neut.txt
+est_dfe -c est_dfe_configfile_sel.txt
+prop_muts_in_s_ranges -c results_dir_sel/est_dfe.out 
+cut  -d' ' -f 3,6,9,12 prop_muts_in_s_ranges.out>output/${sample}.prop_muts_${i}.txt
+done
+cat output/${sample}.prop_muts_*.txt>${sample}_bs.prop_muts.txt
+done
+
+
